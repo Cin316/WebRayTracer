0c653e654b6441b485f88eb37784442dcb328210
Nicholas Reichert <joe13reich@gmail.com>
2019-04-26 23:18:56 -0400

    Started some basic objects for setting up ray tracing.

---
+++ objects/Vector.js
@@ -0,0 +1,11 @@
+class Vector {
+	// number x;
+	// number y;
+	// number z;
+	constructor(x, y, z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+	}
+}
+
--- rayTracer.js
+++ rayTracer.js
@@ -1,3 +1,33 @@
+class Camera {
+	// Vector position;  // The position of the camera measured from the origin.
+	// Vector direction;  // The direction the camera points.  The length of this determines the field of view.
+	// Screen screen;  // The screen is centered at position+direction, perpindicular to direction.
+	constructor(position, direction, screen) {
+		this.position = position;
+		this.direction = direction;
+		this.screen = screen;
+	}
+
+
+	// Add functions to fire vectors and draw an image here.
+
+}
+
+class Screen {
+	// number width;  // The number of units wide this screen is in the 3D world.
+	// number height;  // The number of units high this screen is in the 3D world.
+	// number xRes;  // The number of pixels wide this screen renders to in 2D.
+	// number yRes;  // The number of pixels high this screen renders to in 2D.
+	constructor(width, hieght, xRes, yRes) {
+		this.width = width;
+		this.height = height;
+		this.xRes = xRes;
+		this.yRes = yRes;
+	}
+}
+
+// Create an environment and structures that live in the environment and can return a color if a vector hits them.
+
 
 function main() {
 	var canvas = document.getElementById("canvas");
