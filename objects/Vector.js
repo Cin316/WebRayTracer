9647b14188751bfd2ad6e4645f5078d8960a17fc
Nicholas Reichert <joe13reich@gmail.com>
2019-04-27 22:38:35 -0400

    Added code to generate rays to fire.  Untested.

--- objects/Vector.js
+++ objects/Vector.js
@@ -8,23 +8,36 @@ class Vector {
 		this.z = z;
 	}
 
+	// Returns the sum of this vector with v.
+	// Returns: Vector
+	add(v) {
+		return new Vector(this.x+v.x, this.y+v.y, this.z+v.z);
+	}
+
+	// Returns this vector minus v.
+	// Returns: Vector
+	sub(v) {
+		return new Vector(this.x-v.x, this.y-v.y, this.z-v.z);
+
+	}
+
 	// Returns the norm of this vector.
 	// Returns: number
 	norm() {
-		return Math.sqrt(x*x + y*y + z*z);
+		return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z);
 	}
 
 	// number k;
 	// Multiplies this vector by a scalar k.
 	// Returns: Vector
 	scalarMult(k) {
-		return new Vector(k*x, k*y, k*z);
+		return new Vector(k*this.x, k*this.y, k*this.z);
 	}
 
 	// Returns the unit vector in the direction of this vector.
 	// Returns: Vector
 	unitVector() {
-		return new this.scalarMult(this.norm());
+		return this.scalarMult(1/this.norm());
 	}
 
 	// Vector v;
--- rayTracer.js
+++ rayTracer.js
@@ -2,6 +2,7 @@ class Camera {
 	// Vector position;  // The position of the camera measured from the origin.
 	// Vector direction;  // The direction the camera points.  The length of this determines the field of view.
 	// Screen screen;  // The screen is centered at position+direction, perpindicular to direction.
+
 	constructor(position, direction, screen) {
 		this.position = position;
 		this.direction = direction;
@@ -9,7 +10,38 @@ class Camera {
 	}
 
 
-	// Add functions to fire vectors and draw an image here.
+	// Returns the vector that runs along the width of the camera screen in the 3D world.
+	// Returns: Vector
+	cameraXVec() { 
+		let denom = Math.sqrt(this.direction.x*this.direction.x + this.direction.y*this.direction.y);
+		return (new Vector(-this.direction.y/denom, this.direction.x/denom, 0)).scalarMult(this.screen.width);
+	}
+	// Returns the vector that runs along the height of the camera screen in the 3D world.
+	// Returns: Vector
+	cameraYVec() { 
+		let xVec = this.cameraXVec();
+		return this.direction.cross(xVec).unitVector().scalarMult(this.screen.height);
+	}
+
+	// Returns an array of vectors that reach from the position to the cetner of each pixel, so they can be fired.
+	// Returns: Vector[]
+	getRays() { 
+		let xVec = this.cameraXVec();
+		let yVec = this.cameraYVec();
+		let pixelWidth = xVec.scalarMult(1/this.screen.xRes);
+		let pixelHeight = yVec.scalarMult(1/this.screen.yRes);
+		let centerOffset = pixelWidth.add(pixelHeight).scalarMult(1/2);
+		let upperLeftCorner = this.direction.add(xVec.scalarMult(1/2)).add(yVec.scalarMult(1/2));
+		var array = [];
+		for (var x=0; x<this.screen.xRes; x++) {
+			for (var y=0; y<this.screen.yRes; y++) {
+				let endpoint = upperLeftCorner.add(pixelWidth.scalarMult(x)).add(pixelHeight.scalarMult(y)).add(centerOffset);
+				array.push(endpoint);
+			}
+		}
+
+		return array;
+	}
 
 }
 
@@ -18,7 +50,7 @@ class Screen {
 	// number height;  // The number of units high this screen is in the 3D world.
 	// number xRes;  // The number of pixels wide this screen renders to in 2D.
 	// number yRes;  // The number of pixels high this screen renders to in 2D.
-	constructor(width, hieght, xRes, yRes) {
+	constructor(width, height, xRes, yRes) {
 		this.width = width;
 		this.height = height;
 		this.xRes = xRes;
