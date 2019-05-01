8b666556ecea79751069ac5416d51c89fa47e53d
Nicholas Reichert <joe13reich@gmail.com>
2019-04-30 23:57:21 -0400

    Cleaned up fireRays code.

--- objects/Environment.js
+++ objects/Environment.js
@@ -6,19 +6,19 @@ class Environment {
 		this.geometries = [];
 	}
 
-	// CanvasRenderingContext2D context;
+	// Fires the rays from the camera and makes an array of colored pixels based on where they hit.
 	// Returns: Color[][]
-	fireRays(context) {
+	fireRays() {
 		let xRes = this.camera.screen.xRes;
 		let yRes = this.camera.screen.yRes;
-		// TODO Maybe don't worry about the image data until another place?
-		//var imageData = context.createImageData(this.camera.screen.xRes, this.camera.screen.yRes);
 
 		let rays = this.camera.getRays();
+
 		var colorArray = []; // Color[][] colorArray
 		for (var x=0; x<xRes; x++) {
 			colorArray.push([]);
 			for (var y=0; y<yRes; y++) {
+				// Find the closest hit out of all of the geometries and take that as the color for the pixel.
 				var closestHit = new Contact(false);
 				for (var i=0; i<this.geometries.length; i++) {
 					var newHit = this.geometries[i].evaluateHit(rays[x][y]);
