584e801253a36d06e66a8535de565e36a2706c43
Nicholas Reichert <joe13reich@gmail.com>
2019-05-06 22:20:30 -0400

    Cleaned up scene and added example of using a ColorFunction to make a fancy sphere.

--- objects/Geometries/Sphere.js
+++ objects/Geometries/Sphere.js
@@ -1,7 +1,7 @@
 class Sphere extends Geometry {
 
-	// A ColorFunction takes a Vector position and returns the Color of a collision with that position.
-	// colorFunction: function(Point) -> Color
+	// A ColorFunction takes a Vector position (and a reference to this) and returns the Color of a collision with that position.
+	// colorFunction: function(Vector, Sphere) -> Color
 
 	// Vector center;
 	// number radius;
@@ -11,11 +11,11 @@ class Sphere extends Geometry {
 		this.center = center;
 		this.radius = radius;
 		if (typeof obj == "undefined") { // obj is blank so default to red
-			this.colorFunction = function(point) {
+			this.colorFunction = function(point, that) {
 				return new Color(255, 0, 0);
 			};
 		} else if (typeof obj == "object") { //obj is a Color so display that color everywhere.
-			this.colorFunction = function(point) {
+			this.colorFunction = function(point, that) {
 				return obj;
 			};
 		} else { // Otherwise, obj is a ColorFunction.
@@ -47,8 +47,8 @@ class Sphere extends Geometry {
 			let hit1Location = source.add(ray.scalarMult(t1));
 			let hit2Location = source.add(ray.scalarMult(t2));
 
-			let color1 = this.colorFunction(hit1Location);
-			let color2 = this.colorFunction(hit2Location);
+			let color1 = this.colorFunction(hit1Location, this);
+			let color2 = this.colorFunction(hit2Location, this);
 
 			let hit1 = new Contact(true, t1, color1);
 			let hit2 = new Contact(true, t2, color2);
@@ -62,13 +62,42 @@ class Sphere extends Geometry {
 		} else if (discriminant == 0) { // One solution:
 			let t = -b/(2*a);
 			let hitLocation = source.add(ray.scalarMult(t));
-			let color = this.colorFunction(hitLocation);
+			let color = this.colorFunction(hitLocation, this);
 			let hit = new Contact(true, t, color);
 
 			return hit;
 		} else { // No solutions:
 			return new Contact(false);
 		}
+	}
+
+	// Vector point;
+	// Converts the specified point to a polar coordinate based on this sphere.
+	// The azimuthal angle is the angle in the xy-plane counterclockwise from the positive x-axis.
+	// The angle is given in radians.
+	// This will always be positive, between 0 and 2*pi;
+	// Returns: number
+	pointToAzimuthalAngle(point) {
+		let relativePoint = point.sub(this.center);
+		var angleInRadians = Math.atan(relativePoint.y/relativePoint.x);
+		if (angleInRadians < 0) {
+			angleInRadians = Math.PI*2 + angleInRadians;
+		}
+
+		return angleInRadians;
+	}
+
+	// Vector point;
+	// Converts the specified point to a polar coordinate based on this sphere.
+	// The polar angle is the angle down from the positive z-axis.
+	// The angle is given in radians.
+	// Returns: number
+	pointToPolarAngle(point) {
+		let relativePoint = point.sub(this.center);
+		let angleInRadians = Math.acos(relativePoint.z/this.radius);
+
+		return angleInRadians;
+
 	}
 
 }
--- rayTracer.js
+++ rayTracer.js
@@ -24,14 +24,8 @@ function main() {
 
 	// END Blur fix
 
-	ctx.fillStyle = "red";
+	ctx.fillStyle = "black";
 	ctx.fillRect(0, 0, 330, 170);
-
-	ctx.strokeStyle = "blue";
-	ctx.beginPath();
-	ctx.moveTo(0, 0);
-	ctx.lineTo(30, 30);
-	ctx.stroke();
 
 	var position = new Vector(1, 0, 0);
 	var direction = new Vector(0, 1, 0);
@@ -39,23 +33,36 @@ function main() {
 
 	var cam = new Camera(position, direction, screen);
 
-	var vectors = cam.getRays();
-
 	var environment = new Environment(cam);
 
 	var sphere = new Sphere(new Vector(1, 10, 0), 2, new Color(255, 100, 0));
 	environment.geometries.push(sphere);
-	var sphere2 = new Sphere(new Vector(-1.0, 10, 0), 2, function(point) { return new Color(200,0,0);});
+	var sphere2 = new Sphere(new Vector(-1.0, 10, 0), 2, function(point, that) { return new Color(200,0,0);});
 	environment.geometries.push(sphere2);
 	var sphere3 = new Sphere(new Vector(1.9, 5, 0), 0.25, new Color(0, 50, 150));
 	environment.geometries.push(sphere3);
 	var sphere4 = new Sphere(new Vector(40, 100, 20), 10, new Color(255, 0, 255));
 	environment.geometries.push(sphere4);
 
+	var checkeredSphere = new Sphere(new Vector(25, 80, -5), 10, function(point, that) { 
+		let azimuthalAngle = that.pointToAzimuthalAngle(point);
+		let polarAngle = that.pointToPolarAngle(point);
+		let sectionWidth = (Math.PI*2)/32;
+
+		let sectionPos = azimuthalAngle % (sectionWidth*2);
+
+		//console.log("azimuthalAngle: " + azimuthalAngle + " sectionWidth: " + sectionWidth + " sectionPos: " + sectionPos);
+		if (sectionPos > sectionWidth) {
+			return new Color(255, 255, 0);
+		} else {
+			return new Color(100, 255, 50);
+		}
+	});
+	environment.geometries.push(checkeredSphere);
+
 	var colorArray = environment.fireRays();
 
 	var imageData = convertColorArrToImageData(ctx, colorArray);
-	console.log(imageData);
 
 	ctx.putImageData(imageData, 5, 5);
 
